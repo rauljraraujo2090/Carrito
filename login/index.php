190f82d1cc6bdbdd029926dd2f2d207cba5133ee
vituchin1995 <115170951+vituchin1995@users.noreply.github.com>
2023-09-01 12:54:17 -0500

    log

--- login/index.php
+++ login/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Xacton - Admin & Dashboard Template</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="MyraStudio" name="author" />
@@ -13,9 +13,9 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
     <!-- App css -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 
 </head>
 
@@ -36,8 +36,8 @@
                                                 <i class="mdi mdi-alpha-x-circle"></i> <b>XACTON</b>
                                             </a>h
                                         </div>
-                                        <h1 class="h5 mb-1">Welcome Back!</h1>
-                                        <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
+                                        <h1 class="h5 mb-1">Bienvenido</h1>
+                                        <p class="text-muted mb-4">Ingrese su dirección de correo electrónico y contraseña para acceder al panel de administración.</p>
                                         <form class="user">
                                             <div class="form-group">
                                                 <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
@@ -45,10 +45,10 @@
                                             <div class="form-group">
                                                 <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                                             </div>
-                                            <a href="" class="btn btn-success btn-block waves-effect waves-light"> Log In </a>
+                                            <a href="" class="btn btn-success btn-block waves-effect waves-light">Ingresar</a>
 
                                             <div class="text-center mt-4">
-                                                <h5 class="text-muted font-size-16">Sign in using</h5>
+                                                <h5 class="text-muted font-size-16">Iniciar Sesion usando</h5>
                                             
                                                 <ul class="list-inline mt-3 mb-0">
                                                     <li class="list-inline-item">
@@ -70,8 +70,8 @@
 
                                         <div class="row mt-4">
                                             <div class="col-12 text-center">
-                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Forgot your password?</a></p>
-                                                <p class="text-muted mb-0">Don't have an account? <a href="pages-register.html" class="text-muted font-weight-medium ml-1"><b>Sign Up</b></a></p>
+                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Olvidaste tu contraseña?</a></p>
+                                                <p class="text-muted mb-0">No tienes una cuenta? <a href="pages-register.html" class="text-muted font-weight-medium ml-1"><b>Inscribirse</b></a></p>
                                             </div> <!-- end col -->
                                         </div>
                                         <!-- end row -->
