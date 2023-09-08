84c716837d5e31e956ab6b0bade280cd9919f9f6
vituchin1995 <115170951+vituchin1995@users.noreply.github.com>
2023-09-08 11:58:40 -0500

    aa

--- form.php
+++ form.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,23 +9,38 @@
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 </head>
+
 <body>
     <!-- INICIO MENU -->
     <header id="page-topbar">
+        
         <div class="navbar-header">
-            <div class="d-flex align-items-left">
+            <div class="d-flex align-items-left"></div>
+            <div class="d-flex align-items-center">
                 <div class="dropdown d-inline-block ml-2">
-                    <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="mdi mdi-plus"></i> Usuario
+                    <button type="button" class="btn header-item waves-effect " id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Usuario
                         <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
                     </button>
 
-
-                     
                     <div class="dropdown-menu dropdown-menu-right">
 
-                   </div>
+                        <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
+                            Mi Perfil
+
+
+
+
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
+                                Cerrar Sesion
+
+
+
+
+                                <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
+
+
+                    </div>
 
                 </div>
 
@@ -34,7 +50,17 @@
 
     </header>
     <!-- FIN MENU -->
+<div class="vertical-menu">
+<div  data-simplebar class="h-100">
+<div class="navbar-brand-box">
 
+
+</div>
+
+
+</div>
+
+</div>
     <!-- jQuery  -->
     <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
@@ -46,4 +72,5 @@
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
 
 </body>
+
 </html>
