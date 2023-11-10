b5c05400f5e898c5a5f5b58eacfedffc4532be1a
raul <rauljraraujo2090@gmail.com>
2023-11-10 09:04:24 -0500

    sas

---
+++ include/modal_frm_reg_usuario.php
@@ -0,0 +1,15 @@
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title h4" id="myLargeModalLabel">Large modal</h5>
+                                                    <button type="button" class="close waves-effect waves-light" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    ...
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
