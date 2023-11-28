c31608e2e5a9fba99e9cf2e0b601e04dc98355ab
raul <rauljraraujo2090@gmail.com>
2023-11-28 12:45:13 -0500

    sss

--- productos.php
+++ productos.php
@@ -6,14 +6,23 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productos</title>
+    <title>Carrito de Compras</title>
     <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+
+<!-- Plugins css -->
+    <link href="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/plugins/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/plugins/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/plugins/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
     
-    <link rel="shortcut icon" href="carrito.png">
+
+    
+ 
 </head>
 
 <body>
@@ -408,19 +417,35 @@ echo"</tr>";
 
 
 
+<!-- jQuery  -->
+<script src="plantilla/Admin/assets/js/jquery.min.js"></script>
+    <script src="plantilla/Admin/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="plantilla/Admin/assets/js/metismenu.min.js"></script>
+    <script src="plantilla/Admin/assets/js/waves.js"></script>
+    <script src="plantilla/Admin/assets/js/simplebar.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- third party js -->
+    <script src="plantilla/Admin/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.responsive.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/responsive.bootstrap4.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.buttons.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.bootstrap4.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.html5.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.flash.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.print.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.keyTable.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.select.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/pdfmake.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/vfs_fonts.js"></script>
+    <!-- third party js ends -->
 
-     <!---FIN DE CONTENIDO-->
-    <!-- jQuery  -->
-    <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
-    <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
-
+    <!-- Datatables init -->
+    <script src="plantilla/Admin/vertical/assets/pages/datatables-demo.js"></script>
+    
     <!-- App js -->
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+
 
 </body>
 
