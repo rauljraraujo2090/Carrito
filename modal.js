98cca9a6b8fbefaee2a9027af904505a8465e9f1
raul <rauljraraujo2090@gmail.com>
2023-12-01 12:48:56 -0500

    asas

--- modal.js
+++
@@ -1,7 +0,0 @@
-Swal.fire({
-    position: "top-center",
-    icon: "success",
-    title: "Bienvenido al Carrito de Compras",
-    showConfirmButton: false,
-    timer: 2500
-  });
--- sesion/cerrar_sesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../conexion.php');
-session_start();
-session_destroy();
-header("location:../index.php");
-
-?>
